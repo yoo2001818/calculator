77ff0986b7467f6fcbe603fb85762a586b809ad1
Duknam Yoo <yoo2001818@gmail.com>
2017-05-29 23:04:29 +0900

    Implement shunting yard algorithm

---
+++ src/shuntingYard.js
@@ -0,0 +1,68 @@
+const OPERATORS = {
+  'keyword': 1,
+  '+': 2,
+  '-': 2,
+  '/': 3,
+  '*': 3,
+};
+
+export default function shuntingYard(tokenizer) {
+  let output = [];
+  let stack = [];
+  let tokenState = tokenizer.next();
+  while (!tokenState.done) {
+    let token = tokenState.value;
+    switch (token.type) {
+      case 'number':
+        output.push(token);
+        break;
+      case 'keyword':
+      case '+':
+      case '-':
+      case '/':
+      case '*': {
+        // Compare precedence with top of the stack
+        let peekToken = stack[stack.length - 1];
+        while (peekToken != null &&
+          OPERATORS[peekToken.type] >= OPERATORS[token.type]
+        ) {
+          output.push(stack.pop());
+          peekToken = stack[stack.length - 1];
+        }
+        break;
+      }
+      case '(':
+        stack.push(token);
+        break;
+      case ')': {
+        let pullToken = stack.pop();
+        if (pullToken == null) throw new Error('Unmatched parens');
+        if (pullToken.type === '(') {
+          let peekToken = stack[stack.length - 1];
+          if (peekToken != null && peekToken.type === 'keyword') {
+            output.push(peekToken);
+          }
+        } else {
+          output.push(pullToken);
+        }
+        break;
+      }
+      case ',': {
+        let peekToken = stack[stack.length - 1];
+        while (peekToken != null && peekToken.type !== '(') {
+          output.push(stack.pop());
+          peekToken = stack[stack.length - 1];
+        }
+        break;
+      }
+    }
+    tokenState = tokenizer.next();
+  }
+  // Send all remaining tokens to exit
+  while (stack.length > 0) {
+    let token = stack.pop();
+    if (token.type === '(') throw new Error('Unmatched parens');
+    output.push(token);
+  }
+  return stack;
+}
--- src/tokenize.test.js
+++ src/tokenize.test.js
@@ -2,7 +2,7 @@ import tokenize from './tokenize';
 
 describe('tokenize', () => {
   it('should parse strings correctly', () => {
-    expect(Array.from(tokenize('1 + 5.5 * (6   - 3 / (2 + 3))   '))).toEqual([
+    expect(Array.from(tokenize('1 + 5.5 * (6   - 3 / sin(2 + 3))  '))).toEqual([
       { type: 'number', value: 1 },
       { type: '+' },
       { type: 'number', value: 5.5 },
@@ -12,6 +12,7 @@ describe('tokenize', () => {
       { type: '-' },
       { type: 'number', value: 3 },
       { type: '/' },
+      { type: 'keyword', value: 'sin' },
       { type: '(' },
       { type: 'number', value: 2 },
       { type: '+' },
